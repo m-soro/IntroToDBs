3325573ffdfa4129ae2cff932c1a1a7d120da321
mark-soro <napoleon_soro@rocketmail.com>
2020-08-27 08:21:45 -0400

    added create method to cats

--- cats.js
+++ cats.js
@@ -12,22 +12,44 @@ var Cat = mongoose.model('Cat', catSchema);
 
 // adding a new cat to db
 
-var george = new Cat({
-  name: 'Mrs. Norris',
-  age: 7,
-  temperament: 'Evil'
-});
+// var george = new Cat({
+//   name: 'Mrs. Norris',
+//   age: 7,
+//   temperament: 'Evil'
+// });
 
 // save and define a call back function
-george.save(function(err, cat){
-  if(err){
-    console.log('something went wrong!');
-  } else {
-    console.log('We just saved a cat to the DB:');
-    console.log(cat);
-  }
-});
+// george.save(function(err, cat){
+//   if(err){
+//     console.log('something went wrong!');
+//   } else {
+//     console.log('We just saved a cat to the DB:');
+//     console.log(cat);
+//   }
+// });
 
 // retrieve all cats from db and console.log each one.
 
-//
+Cat.find({}, function(err, cats){
+  if(err){
+    console.log('Oh no, error!');
+    console.log(err);
+  } else {
+    console.log('Here are all the cats');
+    console.log(cats);
+  }
+});
+
+Cat.create({
+  name: 'Snow White',
+  age: 15,
+  temperament: 'Bland',
+}, function(err, newCat){
+  if(err){
+    console.log('Oops, you hit an error!');
+    console.log(err);
+  } else {
+    console.log('Here is the new cat!');
+    console.log(newCat);
+  }
+});
